c1932b91c30b0485b9b935aec71b82a000ad6efc
Sebastijan Horvat <sebastijan.horvatt@gmail.com>
2018-01-26 20:34:00 +0100

    kolokvij

---
+++ app/src/main/java/com/example/sehorva/drugikolokvij/DBAdapter.java
@@ -0,0 +1,188 @@
+package com.example.sehorva.drugikolokvij;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.Console;
+
+/**
+ * Created by sehorva on 1/26/18.
+ */
+
+public class DBAdapter {
+
+    static final String TAG = "DBAdapter";
+    static final String DATABASE_NAME = "MyDB";
+
+    //za tablicu ZAPOSLENIK(ID, IME, MAIL, PLACA
+    static final String DATABASE_TABLE_ZAPOSLENIK = "zaposlenik";
+    static final String KEY_ROWID = "id"; //_id
+    static final String KEY_IME = "ime";
+    static final String KEY_MAIL = "mail";
+    static final String KEY_PLACA = "placa";
+
+    //za tablicu RADNOMJESTO(ID_MJESTA, NIVO_ODGOVORNOSTI, MINIMALNA_PLACA
+    static final String DATABASE_TABLE_RADNOMJESTO = "radnomjesto";
+    static final String KEY_ROWID_MJESTA = "id_mjesta";
+    static final String KEY_NIVO_ODGOVORNOSTI = "nivo_odgovornosti";
+    static final String KEY_MINIMALNA_PLACA = "minimalna_placa";
+
+
+    static final int DATABASE_VERSION = 2;
+
+    static final String DATABASE_CREATE_ZAPOSLENIK =
+            "create table zaposlenik (id integer primary key autoincrement, "
+                    + "ime text not null, mail text not null, placa integer not null);";
+
+    static final String DATABASE_CREATE_RADNOMJESTO =
+            "create table radnomjesto (id_mjesta integer primary key autoincrement, "
+                    + "nivo_odgovornosti integer not null, minimalna_placa integer not null);";
+
+    final Context context;
+
+    DatabaseHelper DBHelper;
+    SQLiteDatabase db;
+
+    public DBAdapter(Context ctx)
+    {
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
+        DatabaseHelper(Context context)
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL(DATABASE_CREATE_ZAPOSLENIK);
+            db.execSQL(DATABASE_CREATE_RADNOMJESTO);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            Log.w(TAG, "Upgrading db from" + oldVersion + "to"
+                    + newVersion );
+            db.execSQL("DROP TABLE IF EXISTS zaposlenik");
+            db.execSQL("DROP TABLE IF EXISTS radnomjesto");
+            onCreate(db);
+        }
+    }
+
+    //---otvara bazu podataka---
+    public DBAdapter open() throws SQLException
+    {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    //---zatvara bazu podataka---
+    public void close()
+    {
+        DBHelper.close();
+    }
+
+    //---ubacuje zaposlenika u tablicu zaposlenik u bazi podataka---
+    public long insertIntoZaposlenik(String ime, String mail, Integer placa)
+    {
+        Log.d("insert", ime+mail+placa);
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_IME, ime);
+        initialValues.put(KEY_MAIL, mail);
+        initialValues.put(KEY_PLACA, placa);
+        return db.insert(DATABASE_TABLE_ZAPOSLENIK, null, initialValues);
+    }
+
+    //---ubacuje radno mjesto u tablicu rednomjesto u bazi podataka---
+    public long insertIntoRadnoMjesto(Integer nivo_odgovornosti, Integer minimalna_placa)
+    {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_NIVO_ODGOVORNOSTI, nivo_odgovornosti);
+        initialValues.put(KEY_MINIMALNA_PLACA, minimalna_placa);
+        return db.insert(DATABASE_TABLE_RADNOMJESTO, null, initialValues);
+    }
+
+    //---brise odredjenog zaposlenika--
+    public boolean deleteZaposlenik(long rowId)
+    {
+        return db.delete(DATABASE_TABLE_ZAPOSLENIK, KEY_ROWID + "=" + rowId, null) > 0;
+
+    }
+
+    //---brise odredjeno radno mjesto--
+    public boolean deleteRadnoMjesto(long rowId)
+    {
+        return db.delete(DATABASE_TABLE_RADNOMJESTO, KEY_ROWID_MJESTA + "=" + rowId, null) > 0;
+    }
+
+    //---vraca sve zaposlenike---
+    public Cursor getSviZaposlenici()
+    {
+        return db.query(DATABASE_TABLE_ZAPOSLENIK, new String[] {KEY_ROWID, KEY_IME,
+                KEY_MAIL, KEY_PLACA}, null, null, null, null, null);
+    }
+
+    //---vraca sva radna mjesta---
+    public Cursor getSvaRadnaMjesta()
+    {
+        return db.query(DATABASE_TABLE_RADNOMJESTO, new String[] {KEY_ROWID_MJESTA, KEY_NIVO_ODGOVORNOSTI,
+                KEY_MINIMALNA_PLACA}, null, null, null, null, null);
+    }
+
+
+    //---vraca odredjenog zaposlenika---
+    public Cursor getZaposlenik(long rowId) throws SQLException
+    {
+        Cursor mCursor =
+                db.query(true, DATABASE_TABLE_ZAPOSLENIK, new String[] {KEY_ROWID,
+                                KEY_IME, KEY_MAIL, KEY_PLACA}, KEY_ROWID + "=" + rowId, null,
+                        null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    //---vraca odredjeno radno mjesto---
+    public Cursor getRadnoMjesto(long rowId_mjesta) throws SQLException
+    {
+        Cursor mCursor =
+                db.query(true, DATABASE_TABLE_RADNOMJESTO, new String[] {KEY_ROWID_MJESTA,
+                                KEY_NIVO_ODGOVORNOSTI, KEY_MINIMALNA_PLACA}, KEY_ROWID_MJESTA + "=" + rowId_mjesta, null,
+                        null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    //---azuriramo (updateamo) zaposlenika---
+    public boolean updateZaposlenik(long rowId, String ime, String mail, Integer placa)
+    {
+        ContentValues args = new ContentValues();
+        args.put(KEY_IME, ime);
+        args.put(KEY_MAIL, mail);
+        args.put(KEY_PLACA, placa);
+        return db.update(DATABASE_TABLE_ZAPOSLENIK, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    //---azuriramo (updateamo) radno mjesto---
+    public boolean updateRadnoMjesto(long rowId_mjesta, Integer nivo_odgovornosti, Integer minimalna_placa)
+    {
+        ContentValues args = new ContentValues();
+        args.put(KEY_NIVO_ODGOVORNOSTI, nivo_odgovornosti);
+        args.put(KEY_MINIMALNA_PLACA, minimalna_placa);
+        return db.update(DATABASE_TABLE_RADNOMJESTO, args, KEY_ROWID_MJESTA + "=" + rowId_mjesta, null) > 0;
+    }
+
+}
---
+++ app/src/main/java/com/example/sehorva/drugikolokvij/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.sehorva.drugikolokvij;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        DBAdapter db = new DBAdapter(this);
+
+        //---donji kod je zakomentiran jer su oni vec dodani u bazu---
+        //---dodavanje zaposlenika---
+        /*db.open();
+        long id = db.insertIntoZaposlenik("Mirko", "mirko34@gmail.com", 2500);
+        id = db.insertIntoZaposlenik("Janko", "janac@hotmail.com", 3560);
+        id = db.insertIntoZaposlenik("Ivana", "ivana.maric@gmail.com", 5000);
+        id = db.insertIntoZaposlenik("Branko", "brahg@hotmail.com", 2800);
+        id = db.insertIntoZaposlenik("Hrvoje", "hrvojko123@gmail.com", 4200);
+        db.close();
+
+        //---dodavanje radnih mjesta---
+        db.open();
+        long id_mjesta = db.insertIntoRadnoMjesto(1, 2800);
+        id_mjesta = db.insertIntoRadnoMjesto(5, 6000);
+        id_mjesta = db.insertIntoRadnoMjesto(3, 4600);
+        id_mjesta = db.insertIntoRadnoMjesto(4, 9000);
+        id_mjesta = db.insertIntoRadnoMjesto(2, 3400);
+        db.close();*/
+
+
+        //--dohvati sve zaposlenike---
+        db.open();
+        Cursor c = db.getSviZaposlenici();
+        if (c.moveToFirst())
+        {
+            do {
+                DisplayZaposlenika(c);
+            } while (c.moveToNext());
+        }
+        db.close();
+
+        //--dohvati sva radna mjesta---
+        db.open();
+        Cursor c2 = db.getSvaRadnaMjesta();
+        if (c2.moveToFirst())
+        {
+            do {
+                DisplayRadnogMjesta(c2);
+            } while (c2.moveToNext());
+        }
+        db.close();
+
+
+
+        //---dohvati zaposlenika---
+        /*db.open();
+        Cursor cu = db.getZaposlenik(2);
+        if (cu.moveToFirst())
+            DisplayZaposlenika(cu);
+        else
+            Toast.makeText(this, "No contact found", Toast.LENGTH_LONG).show();
+        db.close();
+
+        //---dohvati radno mjesto---
+        db.open();
+        Cursor cu2 = db.getRadnoMjesto(2);
+        if (cu2.moveToFirst())
+            DisplayRadnogMjesta(cu2);
+        else
+            Toast.makeText(this, "No contact found", Toast.LENGTH_LONG).show();
+        db.close();*/
+
+
+
+        //---update zaposlenika---
+        /*db.open();
+        if (db.updateZaposlenik(1, "Wei-Meng Lee", "weimenglee@gmail.com", 2000))
+            Toast.makeText(this, "Update successful.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Update failed.", Toast.LENGTH_LONG).show();
+        db.close();
+
+        //---update radnog mjesta---
+        db.open();
+        if (db.updateRadnoMjesto(2, 3, 5000))
+            Toast.makeText(this, "Update successful.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Update failed.", Toast.LENGTH_LONG).show();
+        db.close();*/
+
+
+
+        //---obrisi zaposlenika---
+        /*db.open();
+        if (db.deleteZaposlenik(1))
+            Toast.makeText(this, "Delete successful.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Delete failed.", Toast.LENGTH_LONG).show();
+        db.close();
+
+        //---obrisi radno mjesto---
+        db.open();
+        if (db.deleteRadnoMjesto(1))
+            Toast.makeText(this, "Delete successful.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Delete failed.", Toast.LENGTH_LONG).show();
+        db.close();*/
+    }
+
+    private void DisplayZaposlenika(Cursor c) {
+        /*Toast.makeText(this,
+                "id: " + c.getString(0) + "\n" +
+                        "Ime" + c.getString(1) + "\n" +
+                        "Mail:  " + c.getString(2) + "\n" +
+                        "Placa: " + c.getString(3),
+                Toast.LENGTH_LONG).show();*/
+        TextView textView = (TextView) findViewById(R.id.ispisBaze1);
+        textView.append("\n" + "id: " + c.getString(0) + "\t" +
+                "Ime" + c.getString(1) + "\t" +
+                "Mail:  " + c.getString(2) + "\t" +
+                "Placa: " + c.getString(3));
+    }
+
+    private void DisplayRadnogMjesta(Cursor c) {
+        /*Toast.makeText(this,
+                "id_mjesta: " + c.getString(0) + "\n" +
+                        "Nivo_odgovornosti: " + c.getString(1) + "\n" +
+                        "Minimalna_placa:  " + c.getString(2),
+                Toast.LENGTH_LONG).show();*/
+        TextView textView = (TextView) findViewById(R.id.ispisBaze2);
+        textView.append("\n" + "id_mjesta: " + c.getString(0) + "\t" +
+                "Nivo_odgovornosti: " + c.getString(1) + "\t" +
+                "Minimalna_placa:  " + c.getString(2));
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
